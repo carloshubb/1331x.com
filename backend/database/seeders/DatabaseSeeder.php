37518d7a513c20919a72646d65fba5ecb4250a51
carloslennix <carloslennix38@gmail.com>
2026-02-27 13:05:58 -0400

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,15 +4,5 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    public function run(): void
-    {
-        // no seeders called here
-         $this->call([
-            UsersSeeder::class,
-            //CategoriesSeeder::class,
-            ScriptSettingsSeeder::class,
-        ]);
-    }
+
 }
